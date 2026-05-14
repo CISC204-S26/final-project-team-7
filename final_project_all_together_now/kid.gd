f53004db8591cf9610022eda35cc7cc26f3f8927
cassidyac <cassanne@udel.edu>
2026-05-13 20:26:13 -0400

    i get dinner 😺

--- final_project_all_together_now/kid.gd
+++ final_project_all_together_now/kid.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 @export var player_id: String = "p2"
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0
 
 
 func _physics_process(delta: float) -> void:
