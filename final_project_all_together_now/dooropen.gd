1369dcf6f2e5070a88465616fabb27b3c17a3e45
cassidyac <cassanne@udel.edu>
2026-05-17 21:05:50 -0400

    FINALLY

--- final_project_all_together_now/dooropen.gd
+++ final_project_all_together_now/dooropen.gd
@@ -1,12 +1,8 @@
 extends Area2D
 
-@export var spawn_point: Marker2D
+@export var front_door_sprite: Sprite2D
 
 var player_inside: Node2D = null
-
-@onready var door_close_sprite = $doorclose
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
@@ -16,17 +12,24 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if player_inside and Input.is_action_just_pressed("Interact"):
-		door_close_sprite.visible = false
+		if front_door_sprite and front_door_sprite.visible == false:
+			interact_with_hidden_object()
+		else:
+			print("the door is locked")
+		
+		
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "oldman":
 		player_inside = body
-	
-	
+
+
 
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.name == "oldman":
 		player_inside = null
-		
+
+func interact_with_hidden_object() -> void:
+	get_tree().change_scene_to_file("res://maze.tscn")
